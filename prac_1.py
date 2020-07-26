98a813721c45c229ddcec606d4a17fc4a79b1340
ossang08 <68271074+ossang08@users.noreply.github.com>
2020-07-26 16:53:57 +0900

    practice input, print

---
+++ prac_1.py
@@ -0,0 +1,8 @@
+#학생의 이름, 전공, 학번 입력받아 출력하기
+st_name=input('what is your name?')
+st_major=input('what is your major?')
+st_id=input('what is your student id?')
+#출력하려는 문장
+message=f'Hello, {st_id} {st_name}! Your major is {st_major}'
+#출력
+print(message)
