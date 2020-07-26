0da5143ce48664591fb19b64ef63bbea2f21b753
ossang08 <68271074+ossang08@users.noreply.github.com>
2020-07-26 18:46:53 +0900

    add student_grade.py

--- prac_1.py
+++ prac_1.py
@@ -2,6 +2,7 @@
 st_name=input('what is your name?')
 st_major=input('what is your major?')
 st_id=input('what is your student id?')
+st_grade=int(input('what is your grade?'))
 #출력하려는 문장
 message=f'Hello, {st_id} {st_name}! Your major is {st_major}'
 #출력
